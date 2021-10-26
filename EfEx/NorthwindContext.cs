36c2057b0ad58d0fa4b96f9e69cf9ba622fd4176
Henrik Bulskov <bulskov@ruc.dk>
2021-10-26 11:01:20 +0200

    Mapping example in class

--- EfEx/NorthwindContext.cs
+++ EfEx/NorthwindContext.cs
@@ -21,7 +21,7 @@ namespace EfEx
         {
             base.OnModelCreating(modelBuilder);
 
-            //modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().ToTable("categories");
             modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
             modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
             modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
